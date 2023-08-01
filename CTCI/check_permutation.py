26edacbac09c1228059745b2d9fc07bccaca4f03
Hoang Minh Le <lmhoang.hv@gmail.com>
2023-08-01 22:13:17 +1000

    add quick sort to solution

--- CTCI/check_permutation.py
+++ CTCI/check_permutation.py
@@ -3,9 +3,6 @@ Given two strings,write a method to decide if one is a permutation of the
 other.
 
 '''
-
-
-
 def check_permutation1(string1: str, string2: str) -> bool:
 	'''
 	Using dictionary, check permutation
@@ -111,9 +108,64 @@ def check_permutation2(string1, string2):
 
 	return True
 
-string1 = "sdfasdfdfaaasf"
+# Adding quick sort algorithm
+def partition(string: list, low: int, high: int) ->int:
+
+	pivot = string[high]
+
+	i = low - 1
+
+	for j in range(low, high):
+		if string[j] <= pivot:
+			i += 1
+
+			(string[j], string[i]) = (string[i], string[j])
+
+	(string[i + 1], string[high]) = (string[high], string[i+1])
+
+	return i + 1
+
+
+def quick_sort(string: list, low: int, high: int):
+	if low < high:
+		pi = partition(string, low, high)
+		quick_sort(string, low, pi-1)
+		quick_sort(string, pi+1, high) 
+
+def check_permutation3(string1, string2):
+	'''
+	check permutation of two strings without dictionary
+
+	Parameters:
+		string1 (str): first word
+		string2 (str): second word
+
+	Returns:
+		bool: whether the two words are permutations of each other
+
+	'''
+
+	sorted_string1 = list(string1)
+	sorted_string2 = list(string2)
+
+	quick_sort(sorted_string1, 0, len(string1) -1)
+	quick_sort(sorted_string2, 0, len(string2) -1)
+
+	print(sorted_string1)
+	print(sorted_string2)
+
+	if len(sorted_string1) != len(sorted_string2):
+		return False
+
+	for i in range(len(sorted_string1)):
+		if sorted_string1[i] != sorted_string2[i]:
+			return False
+
+	return True
+
+string1 = "asdfasasafdf"
 
 string2 = "asasdfasafdf"
 
-print(check_permutation1(string1, string2))
-print(check_permutation2(string1, string2))
+
+print(check_permutation3(string1, string2))
