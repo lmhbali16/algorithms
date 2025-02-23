230d44800be72bea3a2551b538f407c8b6c93067
Hoang Minh Le <lmhoang.hv@gmail.com>
2025-02-23 14:33:43 +1100

    factorial zero

--- CTCI/factorial_zero.py
+++ CTCI/factorial_zero.py
@@ -1,7 +1,38 @@
 def factorial_zero(n: int) -> int:
-	p = n - (n % 5)
+	result = 0
 
-	print(p)
+	if n < 5:
+		return 0
+
+	i = 5
+
+	while n // i > 0:
+		result += (n // i)
+		i *= 5
+
+		print(result)
+
+	return int(result)
+
+def factor5(i: int) -> int:
+
+	count  = 0
+
+	while i // 5 > 0:
+		count += 1
+		i //= 5
+
+	return count
+
+def factorial_zero2(n: int) -> int:
+	result = 0
+
+	for i in range(n):
+		if i % 5 == 0:
+			result += factor5(i)
+
+	return int(result)
+	
 
 
-print(5)
+print(factorial_zero2(40))
